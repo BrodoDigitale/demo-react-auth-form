8b3710adaf4a0b325a690515ef4e46b65570df00
Iuliia Sensi <sensi.julia@gmail.com>
2023-06-27 22:02:53 +0200

    feat: manage inputs with useState

--- src/AddUserForm/AddUserForm.js
+++ src/AddUserForm/AddUserForm.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./AddUserForm.module.css";
 import { Card } from "../UI/Card/Card";
 import { Button } from "../UI/Button/Button";
 
 export const AddUserForm = (props) => {
+  const [userData, setUserData] = useState(null);
+
   const addUserHandler = (evt) => {
     evt.preventDefault();
+  };
+  const onChangeHandler = (input, value) => {
+    setUserData((prevState) => {
+      return { ...prevState, [input]: value };
+    });
+    console.log(userData);
   };
   return (
     <Card extraClasses={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          onChange={(evt) => onChangeHandler("username", evt.target.value)}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          onChange={(evt) => onChangeHandler("age", evt.target.value)}
+        />
         <Button type={"submit"}>Add user</Button>
       </form>
     </Card>
