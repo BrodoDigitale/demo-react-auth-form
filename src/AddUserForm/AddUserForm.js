f7bca954cdf645787c7ab6556aa1b0e0428fe469
Iuliia Sensi <sensi.julia@gmail.com>
2023-06-28 22:26:25 +0200

    feat: add validation and 2 way inputs binding

--- src/AddUserForm/AddUserForm.js
+++ src/AddUserForm/AddUserForm.js
@@ -3,29 +3,59 @@ import classes from "./AddUserForm.module.css";
 import { Card } from "../UI/Card/Card";
 import { Button } from "../UI/Button/Button";
 
-export const AddUserForm = (props) => {
+export const AddUserForm = ({ addUserHandler }) => {
   const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredUsernameIsValid, setEnteredUsernameIsValid] = useState(false);
+  const [enteredAgeIsValid, setEnteredAgeIsValid] = useState(false);
   const [enteredAge, setEnteredAge] = useState("");
 
-  const addUserHandler = (evt) => {
+  const submitHandler = (evt) => {
     evt.preventDefault();
-    console.log(enteredUsername, enteredAge);
+    if (enteredUsernameIsValid && enteredAgeIsValid) {
+      addUserHandler({ name: enteredUsername, age: enteredAge });
+    } else {
+      console.log("ERROR INPUT");
+    }
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const nameInputHandler = (evt) => {
+    const username = evt.target.value;
+    setEnteredUsername(username);
+    if (username.trim().length < 3) {
+      setEnteredUsernameIsValid(false);
+    } else {
+      setEnteredUsernameIsValid(true);
+    }
+  };
+
+  const ageInputHandler = (evt) => {
+    const age = evt.target.value;
+    setEnteredAge(age);
+    if (!age || +age < 1) {
+      setEnteredAgeIsValid(false);
+    } else {
+      setEnteredAgeIsValid(true);
+    }
   };
 
   return (
     <Card extraClasses={classes.input}>
-      <form onSubmit={addUserHandler}>
+      <form onSubmit={submitHandler}>
         <label htmlFor="username">Username</label>
         <input
           id="username"
           type="text"
-          onChange={(evt) => setEnteredUsername(evt.target.value)}
+          value={enteredUsername}
+          onChange={(evt) => nameInputHandler(evt)}
         />
         <label htmlFor="age">Age (Years)</label>
         <input
           id="age"
           type="number"
-          onChange={(evt) => setEnteredAge(evt.target.value)}
+          value={enteredAge}
+          onChange={(evt) => ageInputHandler(evt)}
         />
         <Button type={"submit"}>Add user</Button>
       </form>
--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { AddUserForm } from "./AddUserForm/AddUserForm";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const addUserHandler = (newUser) => {
+    setUsers((prevState) => [...prevState, newUser]);
+    console.log(users);
+  };
   return (
     <div>
-      <AddUserForm />
+      <AddUserForm addUserHandler={addUserHandler} />
     </div>
   );
 }
